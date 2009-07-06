b7bab508d4bcd7c1ddc9348e39e950864c604bb4
milk1000cc <info@milk1000.cc>
2009-07-06 12:49:37 +0900

    update spec helper for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
+require 'rubygems'
 require 'spec'
+require 'active_record'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
@@ -6,4 +8,14 @@ require 'acts_as_digested_on'
 
 Spec::Runner.configure do |config|
   
+end
+
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
+
+class ActiveRecord::Base
+  def self.define_table(table_name = self.name.tableize, &migration)
+    ActiveRecord::Schema.define(:version => 1) do
+      create_table(table_name, &migration)
+    end
+  end
 end
