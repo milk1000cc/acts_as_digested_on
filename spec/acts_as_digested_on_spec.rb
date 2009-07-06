a856c1b2c4d5e179c950cf8141535ad84c75c4bd
milk1000cc <info@milk1000.cc>
2009-07-06 12:50:22 +0900

    add spec for ActiveRecord::Base#generate_digest

--- spec/acts_as_digested_on_spec.rb
+++ spec/acts_as_digested_on_spec.rb
@@ -1,7 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+class Article < ActiveRecord::Base
+  define_table do |t|
+    t.string :title
+    t.text :url
+    t.text :digest
+    t.text :content
+  end
+end
+
 describe "ActsAsDigestedOn" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe '#generate_digest' do
+    it 'should generate sha-1 digest of target field with some string' do
+      Article.class_eval do
+        acts_as_digested_on :url
+      end
+
+      url = 'http://example.com'
+      article = Article.new(:url => url)
+      article.generate_digest.should == Digest::SHA1.hexdigest("--#{ url }--")
+    end
   end
 end
