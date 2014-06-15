43b609ad694795747611563cdd7be472c595ca06
milk1000cc <info@milk1000.cc>
2014-06-15 19:26:10 +0900

    Use new rspec syntax

--- spec/acts_as_digested_on_spec.rb
+++ spec/acts_as_digested_on_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
 class Article < ActiveRecord::Base
   define_table do |t|
@@ -11,7 +11,7 @@ class Article < ActiveRecord::Base
   end
 end
 
-describe "ActsAsDigestedOn" do
+describe ActsAsDigestedOn do
   before do
     Article.reset_callbacks :validate
     Article.destroy_all
@@ -21,12 +21,17 @@ describe "ActsAsDigestedOn" do
     @url = 'http://example.com'
   end
 
+  it 'has a version number' do
+    expect(ActsAsDigestedOn::VERSION).not_to be nil
+  end
+
   describe '#generate_digest' do
     it 'should generate sha-1 digest of target field with some string' do
       Article.acts_as_digested_on :url
 
-      article = Article.new(:url => @url)
-      article.generate_digest.should == Digest::SHA1.hexdigest("--#{ @url }--")
+      article = Article.new(url: @url)
+      expect(article.generate_digest).to eq(
+        Digest::SHA1.hexdigest("--#{ @url }--"))
     end
 
     describe 'when more than one target field is given' do
@@ -34,8 +39,9 @@ describe "ActsAsDigestedOn" do
         Article.acts_as_digested_on [:title, :url]
 
         title = 'an article'
-        article = Article.new(:title => title, :url => @url)
-        article.generate_digest.should == Digest::SHA1.hexdigest("--#{ title }--#{ @url }--")
+        article = Article.new(title: title, url: @url)
+        expect(article.generate_digest).to eq(
+          Digest::SHA1.hexdigest("--#{ title }--#{ @url }--"))
       end
     end
   end
@@ -44,21 +50,22 @@ describe "ActsAsDigestedOn" do
     it 'should set "digest" field' do
       Article.acts_as_digested_on :url
 
-      article = Article.new(:url => @url)
-      article.digest.should be_nil
+      article = Article.new(url: @url)
+      expect(article.digest).to be_nil
       article.valid?
-      article.digest.should == article.generate_digest
+      expect(article.digest).to eq article.generate_digest
     end
 
     it 'should set the digest value to :digest_column field when :digest_column option is set' do
-      Article.acts_as_digested_on :url, :digest_column => :my_digest
+      Article.acts_as_digested_on :url, digest_column: :my_digest
 
-      article = Article.new(:url => @url)
-      article.digest.should be_nil
-      article.my_digest.should be_nil
+      article = Article.new(url: @url)
+      expect(article.digest).to be_nil
+      expect(article.my_digest).to be_nil
+
       article.valid?
-      article.digest.should be_nil
-      article.my_digest.should == article.generate_digest
+      expect(article.digest).to be_nil
+      expect(article.my_digest).to eq article.generate_digest
     end
   end
 
@@ -66,40 +73,54 @@ describe "ActsAsDigestedOn" do
     it 'should validate uniqueness of digest field by default' do
       Article.acts_as_digested_on :url
 
-      Article.create! :url => @url
-      proc { Article.create!(:url => @url) }.should raise_error(ActiveRecord::RecordInvalid)
-      proc { Article.create!(:url => 'http://example2.com') }.should_not raise_error
+      Article.create! url: @url
+      expect {
+        Article.create! url: @url
+      }.to raise_error(ActiveRecord::RecordInvalid)
+      expect {
+        Article.create! url: 'http://example2.com'
+      }.not_to raise_error
     end
 
     it 'should carry over the options to the validates_uniqueness_of options' do
-      Article.acts_as_digested_on :url, :scope => :user_id
+      Article.acts_as_digested_on :url, scope: :user_id
 
-      Article.create! :url => @url, :user_id => 1
-      proc { Article.create!(:url => @url, :user_id => 1) }.should raise_error(ActiveRecord::RecordInvalid)
-      proc { Article.create!(:url => @url, :user_id => 2) }.should_not raise_error
-      proc { Article.create!(:url => 'http://example2.com', :user_id => 1) }.should_not raise_error
+      Article.create! url: @url, user_id: 1
+      expect {
+        Article.create! url: @url, user_id: 1
+      }.to raise_error(ActiveRecord::RecordInvalid)
+      expect {
+        Article.create! url: @url, user_id: 2
+      }.not_to raise_error
+      expect {
+        Article.create! url: 'http://example2.com', user_id: 1
+      }.not_to raise_error
     end
 
     it 'should validate uniqueness of digest field when :unique option is true' do
-      Article.acts_as_digested_on :url, :unique => true
+      Article.acts_as_digested_on :url, unique: true
 
-      Article.create! :url => @url
-      proc { Article.create!(:url => @url) }.should raise_error(ActiveRecord::RecordInvalid)
-      proc { Article.create!(:url => 'http://example2.com') }.should_not raise_error
+      Article.create! url: @url
+      expect {
+        Article.create! url: @url
+      }.to raise_error(ActiveRecord::RecordInvalid)
+      expect {
+        Article.create! url: 'http://example2.com'
+      }.not_to raise_error
     end
 
     it 'should not validate uniqueness of digest field when :unique option is false' do
-      Article.acts_as_digested_on :url, :unique => false
+      Article.acts_as_digested_on :url, unique: false
 
-      Article.create! :url => @url
-      proc { Article.create!(:url => @url) }.should_not raise_error
+      Article.create! url: @url
+      expect { Article.create! url: @url }.not_to raise_error
     end
 
     it 'should support string key options' do
       Article.acts_as_digested_on :url, 'unique' => false
 
-      Article.create! :url => @url
-      proc { Article.create!(:url => @url) }.should_not raise_error
+      Article.create! url: @url
+      expect { Article.create! url: @url }.not_to raise_error
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,11 @@
-require 'rspec/core'
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_record'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'acts_as_digested_on'
 
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
-end
+I18n.enforce_available_locales = true
 
-ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3', :database => ':memory:')
 
 class ActiveRecord::Base
   def self.define_table(table_name = self.name.tableize, &migration)
